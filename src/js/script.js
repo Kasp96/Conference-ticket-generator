7d65c988f3620a69869424033e19882f21901031
Kacper <kacperwach96@gmail.com>
2025-01-20 15:23:22 +0100

    Add img change/remove and input validation features

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,81 @@ const inputFile = document.querySelector('.file-input');
 const uploadLabel = document.querySelector('.upload-label');
 const uploadIcon = document.querySelector('.upload-img-icon');
 const uploadText = document.querySelector('.upload-text');
+const hint = document.querySelector('.hint');
+const hintText = hint.querySelector('.hint-text');
+const hintIcon = hint.querySelector('.hint-icon');
+
+const changeUploadImg = (e) => {
+	const file = e.target.files[0];
+	if (file.size > 200 * 1024) {
+		hintIcon.src = '/dist/images/icon-info-error.svg';
+		hintText.textContent = 'File too large. Please upload a photo under 500KB.';
+		hintText.style.color = 'var(--clr-orange-700)';
+		return;
+	}
+	hintIcon.src = '/dist/images/icon-info.svg';
+	hintText.textContent = 'Upload your photo (JPG or PNG, max size: 500KB).';
+	hintText.style.color = 'var(--clr-neutral-500)';
+	const reader = new FileReader();
+
+	reader.onload = (e) => {
+		uploadIcon.src = e.target.result;
+		console.log(e.target.result);
+		uploadIcon.style.width = '50px';
+		uploadIcon.style.height = '50px';
+		uploadIcon.style.padding = '0';
+		uploadIcon.style.border = '1px solid var(--clr-neutral-500)';
+
+		uploadText.style.display = 'none';
+		inputFile.disabled = true;
+
+		if (!document.querySelector('.upload-settings')) {
+			const uploadSettings = document.createElement('div');
+			uploadSettings.classList.add(
+				'upload-settings',
+				'd-flex',
+				'align-items-center',
+				'justify-content-center',
+				'gap-2'
+			);
+			uploadSettings.innerHTML = `
+				<div class="upload-option-box z-1">
+					<label class="remove-img text-decoration-underline">Remove image</label>
+				</div>
+				<div class="upload-option-box z-1">
+					<label class="change-img text-decoration-underline">Change image</label>
+				</div>`;
+			uploadLabel.appendChild(uploadSettings);
+
+			const removeBtn = uploadSettings.querySelector('.remove-img');
+			removeBtn.addEventListener('click', removeUploadImg);
+			const changeImgBtn = uploadSettings.querySelector('.change-img');
+			changeImgBtn.addEventListener('click', changeUploadImgAction);
+		}
+	};
+	reader.readAsDataURL(file);
+};
+
+const changeUploadImgAction = (e) => {
+	e.preventDefault();
+	inputFile.disabled = false;
+	inputFile.click();
+};
+const removeUploadImg = (e) => {
+	e.preventDefault();
+	uploadIcon.src = '/dist/images/icon-upload.svg';
+	uploadIcon.style.width = 'auto';
+	uploadIcon.style.height = 'auto';
+	uploadIcon.style.padding = '';
+	uploadIcon.style.border = '';
+	uploadText.style.display = 'block';
+	const uploadSettings = document.querySelector('.upload-settings');
+	if (uploadSettings) {
+		uploadSettings.remove();
+	}
+	inputFile.value = '';
+	inputFile.disabled = false;
+};
 
 const checkNameInput = () => {
 	const error = nameInput.parentElement.nextElementSibling;
@@ -29,7 +104,6 @@ const checkEmailInput = () => {
 		error.style.display = 'none';
 		emailInput.style.border = '1px solid var(--clr-neutral-500)';
 	} else {
-		console.log(errorText);
 		emailInput.style.border = '1px solid var(--clr-orange-500)';
 		error.style.display = 'flex';
 		errorText.textContent = 'Please enter a valid email address.';
@@ -54,44 +128,27 @@ const checkInputs = () => {
 	checkEmailInput();
 	checkGithubInput();
 };
+const validateInputs = () => {
+	const isNameValid = nameInput.value.length > 3;
+	const isEmailValid = emailRegex.test(emailInput.value);
+	const isGithubValid = githubInput.value.length > 3;
+
+	return isNameValid && isEmailValid && isGithubValid;
+};
 
 const generateTicket = () => {
 	checkInputs();
+	if (validateInputs()) {
+		console.log('jest ok');
+	} else {
+		console.log('nie jest ok');
+	}
 };
 
+inputFile.addEventListener('change', changeUploadImg);
 generateBtn.addEventListener('click', generateTicket);
 nameInput.addEventListener('keydown', (e) => {
 	if (e.key >= '0' && e.key <= '9') {
 		e.preventDefault();
 	}
-});
-inputFile.addEventListener('change', (e) => {
-	const file = e.target.files[0];
-	const reader = new FileReader();
-	reader.onload = (e) => {
-		uploadIcon.src = e.target.result;
-		uploadIcon.style.width = '50px';
-		uploadIcon.style.height = '50px';
-		uploadIcon.style.padding = '0';
-		uploadIcon.style.border = '1px solid var(--clr-neutral-500)';
-		uploadText.style.display = 'none';
-		const uploadSettings = document.createElement('div');
-		uploadSettings.classList.add(
-			'upload-settings',
-			'd-flex',
-			'align-items-center',
-			'justify-content-center',
-			'gap-2'
-		);
-		uploadSettings.innerHTML = `<div class="upload-option-box z-1">
-                        <label class="remove-img text-decoration-underline" for="filInput">Remove image</label>
-                        <input class="upload-option d-none" type="file">
-                    </div>
-                    <div class="upload-option-box z-1">
-                        <label class="change-img" for="filInput">Change image</label>
-                        <input class="upload-option d-none" type="file">
-                    </div>`;
-		uploadLabel.appendChild(uploadSettings);
-	};
-	reader.readAsDataURL(file);
 });
